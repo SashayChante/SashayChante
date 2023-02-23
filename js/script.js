860366c850184b41bc73d7527042f9417f761a43
SashayChante <125630966+SashayChante@users.noreply.github.com>
2023-02-23 21:14:31 +1300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,3 +2,15 @@
   function myFunction(x) {
     x.classList.toggle("change");
   }
+
+//JAVASCRIPT 4 - PHONE NUMBER FORMATTING
+//Implements security features
+"use strict";
+document.getElementById("phone").addEventListener("keydown", function(e) {
+  const txt = this.value;
+  // prevent more than 14 characters, ignore the spacebar, allow the backspace
+  if ((txt.length == 14 || e.which == 32) & e.which !== 8) e.preventDefault();
+  // add spaces after 3 & 7 characters, allow the backspace
+  if ((txt.length == 3 || txt.length == 7) && e.which !== 8)
+    this.value = this.value + " ";
+});
